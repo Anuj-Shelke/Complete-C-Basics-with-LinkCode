cfd16e9d1d97df969d9cd2df7fc19cc4148f2d6e
Anuj Shelke <anujshelke18@gmail.com>
2026-07-27 18:25:16 +0530

    Code to print all even number from 0 to 10  ;

--- first.c
+++ first.c
@@ -1,16 +1,16 @@
 // While loop Link_code C language
-// Code to print all even number from 1 to 20 ;
+// Code to print all even number from 0 to 10  ;
 
 #include <stdio.h>
 
 int main()
 {
 
-    int i = 2;
-    while (i <= 20)
+    int i = 10 ;
+    while (i >= 0 )
     {
         printf("%d \n", i);
-        i += 2;
+        i--;
     }
     return 0;
 }
