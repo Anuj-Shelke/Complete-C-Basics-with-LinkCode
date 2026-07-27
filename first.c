f3402eddbc78f4cf3a3496a4b1c570b204beaf9d
Anuj Shelke <anujshelke18@gmail.com>
2026-07-27 19:41:25 +0530

    // Code to Check if a number is a prime number Optimized Approach   ;

--- first.c
+++ first.c
@@ -1,23 +1,33 @@
 // While loop Link_code C language
 // Code to Check if a number is a prime number  ;
+// While loop Link_code C language
+// Code to Check if a number is a prime number  ;
 #include<stdio.h>
 int main(){
     int count = 0 ; 
     int i = 1 ; 
     int n = 0 ; 
-    printf("Enter number to check if a number is prime or not : ");
+    printf("Enter number to check if a number is prime or not : \n ");
     scanf("%d",&n); 
     while(i <=  n ){
         
      if(n%i  == 0 ){
-         count++; 
+          count++; 
+        if(count >  3){
+            break; 
+        }
+         
      }
      i++; 
      
      
     }
     if(count ==2 ){
-        printf("The number is a prime number "); 
+        printf("The number is a prime number \n "); 
+    }
+    else{
+        printf("The number is not prime number "); 
+        
     }
     return 0; 
 }
