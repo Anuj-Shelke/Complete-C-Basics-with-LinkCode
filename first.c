41325587e3681707a5abd3516307d4246b5ce9fa
Anuj Shelke <anujshelke18@gmail.com>
2026-07-27 19:08:57 +0530

    //Code to orint factors of a given number ;

--- first.c
+++ first.c
@@ -1,12 +1,16 @@
 // While loop Link_code C language
-#include <stdio.h>
-int main()
-{
-    int ch = 65;
-
-    while (ch <= 90)
-    {
-        printf("%d : %c | %d : %c \n", ch, ch, ch + 32, ch + 32);
-        ch++;
+//Code to orint factors of a given number ; 
+#include<stdio.h> 
+int main(){
+    int i = 1 ; 
+    int n = 0 ; 
+    printf("Enter the value of n "); 
+    scanf("%d",&n); 
+    while(i<=n){
+        if(n%i == 0 ){
+            printf("The factor of the number is %d is %d \n",n ,i); 
+            
+        }
+        i++; 
     }
 }
