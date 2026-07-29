1b1b484b3bd9f77732f1c0a545c2ca3eb41101e9
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 22:35:00 +0530

    Code to print multiplication table of a given number using For Loop

--- first.c
+++ first.c
@@ -1,47 +1,13 @@
-#include<stdio.h>
-int main(){
-    printf("----------------------Factors-Code------------------\n"); 
-    int count =0 ; 
-    //Factor of a number using for loop 
-    int n = 1 ; 
-    int temp = 1 ; 
-    printf("Enter the value of n \n"); 
+// Code to print multiplication table of a given number using For Loop 
+#include <stdio.h>
+
+int main() {
+    int n = 0 ; 
+    printf("Enter number to print multiplication table of :"); 
     scanf("%d",&n); 
-    for(int i = 1  ; i <= n/2 ; i++){
-        if(n%i== 0){
-            printf("The factor of number is %d\n",i);  
-        }
-       
+    for(int i = 1  ; i<= 10 ; i++){
+        printf("%d X %d = %d\n",i,n,i*n); 
         
     }
-    
-    printf("---------------------------Factorial-----------------\n");
-    int fact = 1; 
-    int numm = 0 ; 
-    printf("Enter the value of n \n"); 
-    scanf("%d",&numm);
-    for(int i = 1 ; i<=  numm ; i++){
-        fact = fact * i; 
-    }
-    
-    printf("The factorial of the number is %d\n",fact);
-    
-    printf("------------------------------Prime Number-----------------\n"); 
-    int num = 0 ; 
-    int co = 0 ; 
-    printf("Enter the value to check if it is a prime number \n");
-    scanf("%d",&num); 
-    for(int i = 2   ; i < num/2 ;  i++){
-        if(num%i == 0 ){
-          co = 1;  
-        }
-        
-    }
-    if(co ==  0 ){
-        printf("The number is  prime number \n"); 
-    }
-    else{
-        printf("The number is not a prime number \n"); 
-    }
-    
+    printf("Thank You"); 
 }
