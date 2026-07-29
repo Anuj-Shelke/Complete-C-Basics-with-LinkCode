040e3c04a7b4eb06a0ffbdd42156f59d0faac8e1
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 19:26:32 +0530

    Code to calculate Factor,Factorial,Prime Number

--- first.c
+++ first.c
@@ -1,19 +1,47 @@
 #include<stdio.h>
 int main(){
-    printf("Numbers from 1 to 10 using for loop \n");
-    int n = 10 ; 
-    for(int i = 1 ; i < n ; i++){
-        printf("%d\n",i);
+    printf("----------------------Factors-Code------------------\n"); 
+    int count =0 ; 
+    //Factor of a number using for loop 
+    int n = 1 ; 
+    int temp = 1 ; 
+    printf("Enter the value of n \n"); 
+    scanf("%d",&n); 
+    for(int i = 1  ; i <= n/2 ; i++){
+        if(n%i== 0){
+            printf("The factor of number is %d\n",i);  
+        }
+       
         
     }
-    printf("Now printing from 10 to  1 \n");
-    //Print number from 1 to 10 
-    for(int i = 10 ; i >=1; i--){
-        printf("%d\n",i); 
+    
+    printf("---------------------------Factorial-----------------\n");
+    int fact = 1; 
+    int numm = 0 ; 
+    printf("Enter the value of n \n"); 
+    scanf("%d",&numm);
+    for(int i = 1 ; i<=  numm ; i++){
+        fact = fact * i; 
     }
-    printf("ASCII Values \n"); 
-    for(int i = 65 ; i < 90 ; i++){
-        printf("%d : %c\n",i,i); 
+    
+    printf("The factorial of the number is %d\n",fact);
+    
+    printf("------------------------------Prime Number-----------------\n"); 
+    int num = 0 ; 
+    int co = 0 ; 
+    printf("Enter the value to check if it is a prime number \n");
+    scanf("%d",&num); 
+    for(int i = 2   ; i < num/2 ;  i++){
+        if(num%i == 0 ){
+          co = 1;  
+        }
+        
+    }
+    if(co ==  0 ){
+        printf("The number is  prime number \n"); 
+    }
+    else{
+        printf("The number is not a prime number \n"); 
     }
     
 }
