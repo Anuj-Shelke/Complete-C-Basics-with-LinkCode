e615a0df95e0cbdcda6de8cb83523b41f50b39b0
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 08:55:32 +0530

    // Code to check if a number is a pallindrome;

--- first.c
+++ first.c
@@ -1,13 +1,20 @@
-// Code to reverse a number ; 
+// Code to check if a number is a pallindrome; 
 #include<stdio.h>
 int main(){
-    int n, temp; 
+    int n, temp,save ; 
     temp = 0 ; 
-    printf("Enter number to be reversed "); 
+    printf("Enter number to be reversed \n"); 
     scanf("%d",&n); 
+    save = n ; 
     while(n != 0){
         temp = temp*10+(n%10); 
         n=n/10; 
     }
-    printf("The number reversed is %d",temp); 
+    printf("The number reversed is %d \n ",temp); 
+    if(temp == save){
+        printf("The given number is a pallindrome \n"); 
+    }
+    else{
+        printf("The number is not a pallindrome\n");
+    }
 }
