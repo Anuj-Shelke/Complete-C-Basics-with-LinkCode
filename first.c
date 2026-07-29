095622d799f71df4d7062b3677676b1a3e83b6c1
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 09:05:12 +0530

    / Code to Enter Base and power and enter their result

--- first.c
+++ first.c
@@ -1,20 +1,17 @@
-// Code to check if a number is a pallindrome; 
-#include<stdio.h>
+// Code to Enter Base and power and enter their result 
+#include <stdio.h>
 int main(){
-    int n, temp,save ; 
-    temp = 0 ; 
-    printf("Enter number to be reversed \n"); 
-    scanf("%d",&n); 
-    save = n ; 
-    while(n != 0){
-        temp = temp*10+(n%10); 
-        n=n/10; 
+    int base,power ;
+    int temp = 1; 
+    int count = 0 ; 
+    printf("Enter power "); 
+    scanf("%d",&power); 
+    printf("Enter base "); 
+    scanf("%d",&base); 
+    while(count != power){
+        temp = base* temp; 
+        count++; 
     }
-    printf("The number reversed is %d \n ",temp); 
-    if(temp == save){
-        printf("The given number is a pallindrome \n"); 
-    }
-    else{
-        printf("The number is not a pallindrome\n");
-    }
+    printf("The result of the calculation is %d\n",temp); 
+    
 }
