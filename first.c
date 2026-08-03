6c20269eccad3414b93a4f7c90f1d868c953e008
Anuj Shelke <anujshelke18@gmail.com>
2026-08-03 23:59:35 +0530

    Another Pattern

--- first.c
+++ first.c
@@ -1,10 +1,10 @@
 /*
 code to print this pattern
- * * * * * 
-  * * * *
-   * * *
-    * *
-     *   
+*****
+ ****
+  ***
+   **
+    * 
    
   
  
@@ -12,18 +12,20 @@ code to print this pattern
 #include <stdio.h>
 
 int main() {
+  int rem = 1;
   int row = 1; 
   int coln = 1; 
   for(row = 1; row <= 5 ; row++){
-      for(int space = 1 ; space <= row; space++){
+      for(int space = 2; space <= row; space++){
           printf(" "); 
           
       }
-      for(coln = 5; coln >= row ; coln--){
-          printf(" *"); 
+
+      for(coln = 5; coln >= row; coln--){
+          printf("*"); 
       }
       printf("\n");
-      
+   
       
   }
   
