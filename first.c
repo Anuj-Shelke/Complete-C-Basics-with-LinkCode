f7892385b9b9b01faadc633f6fef47c02574f02b
Anuj Shelke <anujshelke18@gmail.com>
2026-08-03 18:44:51 +0530

    Code to print given pattern

--- first.c
+++ first.c
@@ -1,23 +1,68 @@
-//Code to Print Star Pattern 
+//Code to Print this pattern
+// 12345
+// 1234
+// 123
+// 12
+// 1
+// 12
+// 123
+// 1234
+// 12345
 #include <stdio.h>
 
 int main() {
-    // int ch = 65;
-for(int row = 1 ; row <=5 ; row++){
-    for(char coln = 1; coln <= row; coln++){
-        printf("*");
+    int i = 1 ;
+for(int row = 5 ; row >= 0; row--){
+    i = 1;
+    for( int coln = 1; coln <= row; coln++){
+        printf("%d",i);
+        i++; 
+    }
+    if(row == 1){
+        continue;
+        
+    }
+    else{
+        printf("\n"); 
+    }
+    
+}
+for(int row = 2 ; row <= 5; row++){
+    i = 1;
+    for( int coln = 1; coln <= row; coln++){
+        printf("%d",i);
+        i++; 
     }
     printf("\n"); 
-    ch++; 
+    
 }
-ch = ch-2; 
-for(int row = 4 ; row >= 0 ; row--){
-    for(char coln = 1; coln <= row; coln++){
-        printf("*");
+
+
+for(int row = 0 ; row <= 5 ; row++){
+    i = 1;
+    for( int coln = 1; coln <= row; coln++){
+        printf("%d",i);
+        i++; 
+    }
+    // if(row == 1){
+    //     continue;
+        
+    // }
+    // else{
+        printf("\n"); 
+    // }
+    
+}
+for(int row = 4 ; row >= 0; row--){
+    i = 1;
+    for( int coln = 1; coln <= row; coln++){
+        printf("%d",i);
+        i++; 
     }
     printf("\n"); 
-    ch++; 
+    
 }
+
 
 
 
