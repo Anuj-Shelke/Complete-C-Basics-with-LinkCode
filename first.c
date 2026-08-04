1b563a6159ddb7242a506605078fe7aa771d0ab1
Anuj Shelke <anujshelke18@gmail.com>
2026-08-04 18:36:20 +0530

    Code to print Alphabetical pyramid  with space

--- first.c
+++ first.c
@@ -1,13 +1,6 @@
 /*
-Code to print this pattern with space
-   * * * * * * * * *
-     * * * * * * *
-       * * * * *
-         * * *
-           *
-        
-   
-  */
+Code to print Alphabetical pyramid  with space
+*/
 
 #include <stdio.h>
 
@@ -15,25 +8,28 @@ int main() {
     int row = 1; 
     int coln = 1; 
     int sp = 1; 
+    int i =  65; 
     
-    // for(row = 0 ; row < 5; row++){
-    //     for(sp = 5 ; sp > row; sp--){
+    for(row = 0 ; row < 5 ; row++){
+        for(sp = 5 ; sp > row; sp--){
+            printf("  ");
+        }
+        for(coln = 0; coln <= row*2; coln++){
+            printf(" %c",i); 
+            
+        }
+        i++; 
+        printf("\n"); 
+    }
+    
+    // for(row = 1  ; row < 5; row++){
+    //     for(sp = 0  ; sp <=row ; sp++){
     //         printf("  ");
     //     }
-    //     for(coln = 0; coln <= row*2; coln++){
+    //     for(coln = 9 ; coln >row*2; coln--){
     //         printf(" *"); 
     //     }
     //     printf("\n"); 
     // }
-    
-    for(row = 0  ; row < 5; row++){
-        for(sp = 0  ; sp <=row ; sp++){
-            printf("  ");
-        }
-        for(coln = 9 ; coln >row*2; coln--){
-            printf(" *"); 
-        }
-        printf("\n"); 
-    }
     return 0;
 }
