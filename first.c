3d05ee340849efa2726ec918f5f82e7a499d841c
Anuj Shelke <anujshelke18@gmail.com>
2026-08-04 08:45:10 +0530

    Code to Print the given pattern

--- first.c
+++ first.c
@@ -1,34 +1,61 @@
-/*
-code to print this pattern
-*****
- ****
-  ***
-   **
-    * 
-   
-  
- 
-*/
+
 #include <stdio.h>
-
+/*code to print this pattern{
+     *
+    ***
+  *****
+  *******
+ *********
+  *******
+   *****
+    ***
+     *
+    
+}
+     */
+  
 int main() {
-  int rem = 1;
-  int row = 1; 
-  int coln = 1; 
-  for(row = 1; row <= 5 ; row++){
-      for(int space = 2; space <= row; space++){
-          printf(" "); 
+    int row = 0; 
+    int space = 0; 
+    int coln = 0 ; 
+    
+    for(row = 0 ; row < 4 ; row++){
+        for(space=4  ; space >= row ; space--){
+            printf(" "); 
           
-      }
-
-      for(coln = 5; coln >= row; coln--){
-          printf("*"); 
-      }
-      printf("\n");
+        }
+        
+        
+        for(coln = 0; coln <= 2*row ; coln++){
+            printf("*");
+        }
+        
+        space = 0; 
+        // if(space){
+            // printf("\n"); 
+        // }
+        
+        
+        // for(coln = 10; coln <= 2*row; coln--){
+        //     printf("$");
+        // }
+        printf("\n");
+        
+        
+    }
+    // printf("**********");
+    
+    for(row = 0 ; row < 5 ; row++){
+    
    
-      
-  }
-  
-  
+  for(space=0 ; space <= row ; space++){
+            printf(" "); 
+        }
+       for(coln = 8; coln >= row*2; coln--){
+            printf("*");
+        }
+        printf("\n"); 
+    }
+
     return 0;
 }
