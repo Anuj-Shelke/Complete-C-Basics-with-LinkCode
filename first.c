f5cf49b18c58286fc8cebd56964002c89c2e3f0f
Anuj Shelke <anujshelke18@gmail.com>
2026-08-04 21:30:52 +0530

    Code to print Empty Arrow

--- first.c
+++ first.c
@@ -1,43 +1,40 @@
-/*
-Code to print this pattern with space
-*********
-  *******
-    *****
-      ***
-        *                          
-   
-  */
-
+// Online C compiler to run C program online
 #include <stdio.h>
+/*
+Code to print Empty Arrow
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+
+
+*/
 
 int main() {
     int row = 1; 
     int coln = 1; 
     int sp = 1; 
-
-    
-    for(row = 0 ; row < 5 ; row++){
-        for(sp = 0 ; sp < row; sp++){
-            printf("  ");
-        }
-        for(coln = 8; coln >= row*2; coln--){
+    for(row = 5; row >=1 ; row--){
+        for(sp =1; sp<=row ; sp++){
             printf("*"); 
-            
         }
-       
+        for(int s = 9; s>=row*2 ; s--){
+            printf(" "); 
+        }
+        for(coln = 1 ; coln <=row; coln++){
+            printf("*"); 
+        }
+        
         printf("\n"); 
+        
+    }
+    for(row = 1; row <= 5; row++){
+        
+        printf("\n");
     }
     
-    // for(row = 1 ; row <= 26; row++){
-    //     for(sp = 1  ; sp <=row ; sp++){
-    //         printf("  ");
-    //     }
-        
-    //     for(coln = 27; coln >row*2; coln--){
-    //         printf(" %c",i); 
-    //     }
-    //     i--; 
-    //     printf("\n"); 
-    // }
+    
     return 0;
 }
