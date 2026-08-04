88ab8c0460bfe8b4b41f3c63aab579ec82f363df
Anuj Shelke <anujshelke18@gmail.com>
2026-08-04 17:59:13 +0530

    Code to print Star Pyramid with space

--- first.c
+++ first.c
@@ -1,40 +1,28 @@
+/*
+Code to print this pattern with space
+           *
+         * * *
+       * * * * *
+     * * * * * * *
+   * * * * * * * * *
+   
+  */
 
 #include <stdio.h>
-/*code to print this pattern{
-     1
-    121
-   12121
-  1212121
- 121212121
-    
-}
-     */
-  
+
 int main() {
-    int row = 0; 
-    int space = 0; 
-    int coln = 0 ; 
-    int i = 1; 
-    for(row = 0 ; row < 5 ; row++){
-        for(space=4  ; space >= row ; space--){
-            printf(" "); 
-          
-        }
-        
-        i = 1; 
-        for(coln = 0 ; coln <= row*2 ; coln++){
-            if(coln% 2 == 0  ){
-                i = 1; 
-            }
-            printf("%d",i);
-            i++; 
-        }
-     
-     
-        printf("\n");
-        
-        
-    }
+    int row = 1; 
+    int coln = 1; 
+    int sp = 1; 
     
+    for(row = 0 ; row < 5; row++){
+        for(sp = 5 ; sp > row; sp--){
+            printf("  ");
+        }
+        for(coln = 0; coln <= row*2; coln++){
+            printf(" *"); 
+        }
+        printf("\n"); 
+    }
     return 0;
 }
