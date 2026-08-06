85d995879fffb904f803b3561b5963ae87dea06c
Anuj Shelke <anujshelke18@gmail.com>
2026-08-06 18:25:05 +0530

    Code for dinding area of shapes using do while

--- first.c
+++ first.c
@@ -4,53 +4,59 @@
 int main() {
     int n1 ;
     int n2 ; 
-    int add ;
+    int area_circle ; 
+    int area_square; 
+    int area_triangle ;
     int choice = 1;
     int ct = 1; 
-    int sub ; 
-    int mul ; 
-    int div ; 
+    int height ; 
+    int base  ; 
+    int r  ; 
+    int side ; 
     
     
     
    
    do{
-        printf("Enter number 1 \n"); 
-        scanf("%d",&n1);
-        printf("Enter number 1 \n"); 
-        scanf("%d",&n2);
+        
+       
     
-       printf("Enter 1 for addtion \n"); 
-       printf("Enter 2 for substraction \n"); 
-       printf("Enter 3 for multiplication \n"); 
-       printf("Enter 4 for division \n"); 
+       printf("Enter 1 for area of circle \n"); 
+       printf("Enter 2 for area of triangle  \n"); 
+       printf("Enter 3 for square \n"); 
+       
        scanf("%d",&choice);
        switch(choice){
            case 1: {
-               add = n1+n2; 
-               printf("Addition of the number %d and %d is %d \n",n1,n2,add);
+               printf("Enter Radius \n"); 
+               scanf("%d",&r);
+               area_circle = 3.14*r*r; 
+               printf("Area of circle is  %d \n",area_circle);
                break; 
            }
           case 2 :{
-              sub = n1-n2; 
-              printf("substraction  of the number %d and %d is %d \n",n1,n2,sub); 
-              break ; 
+              
+              printf("Enter base \n"); 
+              scanf("%d",&base); 
+              printf("Enter height \n"); 
+              scanf("%d",&height); 
+              area_triangle = 0.5*base*height; 
+              printf("Area of Triangle %d\n",area_triangle);
+              break; 
           }
           case 3 :{
-             mul = n1*n2; 
-              printf("multiplication  of the number %d and %d is %d \n",n1,n2,mul); 
+              printf("Enter side of square  \n"); 
+              scanf("%d",&side); 
+             area_square= side*side; 
+              printf("Area of square is %d \n",area_square); 
               break; 
           }
-         case 4:{
-             
-             div = n1/n2; 
-              printf("division of the number %d and %d is %d \n",n1,n2,div); 
-              break; 
-         }
+        
        }
        printf("Enter 1 if you want to continue the program\n"); 
        scanf("%d",&ct); 
-   }while(ct == 1); 
+   }while(ct == 1);
+   printf("--------------Thank----------------- \n"); 
 
     return 0;
 }
