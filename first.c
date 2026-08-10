c280f54562cdad7967d05a4f6da43025a4710ae2
Anuj Shelke <anujshelke18@gmail.com>
2026-08-10 23:11:06 +0530

    Code to find the second greatest element in an array

--- first.c
+++ first.c
@@ -1,4 +1,5 @@
-// Code to take ten numbers from user and check if they are even or odd 
+// Code to take find second Greatest element in an array 
+
 #include <stdio.h>
 
 int main() {
@@ -6,7 +7,7 @@ int main() {
     int min = 0 ; 
     int avg = 0 ; 
     int sum = 0 ; 
-    printf("Enter any 10 numbers to check if they are even or odd :\n");
+    printf("Enter any 5  numbers to check if they are even or odd :\n");
     int arr[5]; 
     for(int i = 0 ;i< 5; i++){
          scanf("%d",&arr[i]); 
@@ -15,21 +16,27 @@ int main() {
     min = arr[0];
     max = arr[0]; 
     for(int i = 0 ; i < 5 ; i++){
-          sum = sum+ arr[i];
+         
       if (  arr[i]> max ){
          max = arr[i];
       }
-      if(arr[i]<min ){
-         min = arr[i]; 
-      }
+      
+      
      
        
     }
-    printf("The sum of the array element is %d\n",sum);
-    printf("The avg of the array element is %d\n",sum/5); 
-     printf("the max of the array element is %d\n",max); 
+    int max_2 = 0 ; 
+    for(int i = 0 ; i< 5 ; i++){
+      if(arr[i]>max_2 && arr[i]<max){
+         max_2= arr[i]; 
+    }
+
+   }
+   
+
+     printf("the second greatest elemnent in an Array is  %d\n",max_2); 
      
-     printf("The min of the array element is %d\n",min); 
+     
     
    
     return 0;
