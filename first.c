18764377a0bb93289ad9dc35e8f8e2f4cbd2f905
Anuj Shelke <anujshelke18@gmail.com>
2026-08-10 18:06:26 +0530

    / Code to print array element using for loop in reverse order and then printing sum of given array element

--- first.c
+++ first.c
@@ -1,1 +1,13 @@
-// Array is the collection of similar data type storing data continously and defined by using a common name each element of an array access by using index or subscript number and index starts from zero ; 
+// Code to print array element using for loop in reverse order and then printing sum of given array element
+#include <stdio.h>
+
+int main() {
+    int sum = 0 ; 
+  int arr[10]={1,2,3,4,5,6,7,8,9,10};
+  for(int i = 9 ; i >=0 ; i--){
+      printf("%d\n",arr[i]); 
+      sum = sum + arr[i]; 
+  }
+  printf("Sum of array elements is %d",sum); 
+    return 0;
+}
