863c8c60fc0457b2bb93b327ec1d107a618ddd7a
Anuj Shelke <anujshelke18@gmail.com>
2026-08-10 18:48:45 +0530

    Code to check count of numbers divisible by 5 and 3 and individually as well

--- first.c
+++ first.c
@@ -2,20 +2,38 @@
 #include <stdio.h>
 
 int main() {
+    int num=0; 
+    int only_3; 
     int count = 0 ; 
+    int only_5 = 0 ; 
     printf("Enter any 10 numbers to check if they are even or odd :\n");
     int arr[10]; 
     for(int i = 0 ;i< 10; i++){
          scanf("%d",&arr[i]); 
     }
     for(int i = 0 ; i < 10 ; i++){
-        if(arr[i]%2==0){
+        if((arr[i]%3==0)&&(arr[i]%5==0)){
             count++; 
         }
-       
+        else if(arr[i]%3==0){
+            only_3++; 
+        }
+        else if(arr[i]%5==0){
+
+            only_5++; 
+        }
+        else{
+            num++; 
+        }
     }
-     printf("there are total %d even numbers in array\n",count); 
-     printf("There are total %d odd numbers in array\n",10-count); 
+
+     printf("there are total %d  numbers in array divisible by 3 and 5 \n",count); 
+     printf("There are total %d numbers in array divisible by 3 only\n",only_3); 
+     printf("There are total %d numbers in array divisible by 5 only\n",only_5); 
+     printf("There are total %d numbers in array divisible by  non of them \n",num); 
+     
+     
+     
    
     return 0;
 }
