a0ceb45d0390aaa0775c66bdc063f14bcd34ecee
Anuj Shelke <anujshelke18@gmail.com>
2026-08-11 18:55:06 +0530

    Code to print sorted array elements

--- first.c
+++ first.c
@@ -2,53 +2,21 @@
 #include <stdio.h>
 
 int main() {
-    int arr[5] ; 
-    int even[5]; 
-    int odd[5]; 
-    int e_count =0 ; 
+    int temp = 0 ; 
+    int arr[5]={6,5,4,3,2}; 
+    for(int i = 0 ; i < 5-1 ; i++){
+        for(int j = i+1; j < 5 ; j++){
+            if(arr[j] < arr[i]){
+                temp = arr[j]; 
+                arr[j] = arr[i]; 
+                arr[i] = temp; 
 
-    printf("Enter array elements "); 
-    for(int i = 0 ; i < 5; i++){
-        scanf("%d",&arr[i]);
-
-    }
-    for(int i = 0; i < 5; i++){
-        if(arr[i] % 2== 0 ){
-            even[i] = arr[i] ; 
-            e_count++; 
-        }
-        else{
-            odd[i] = arr[i]; 
-
-            
+            }
         }
     }
-    printf("There are total %d Even Numbers \n",e_count); 
-    printf("The even Array elements are : \n");
-    for(int i = 0 ; i < 5 ; i++){
-        if(even[i]==0){
-            continue; 
-        }
-        else{
-        printf("%d\n",even[i]); 
-        }
-        
-    
+    for(int i =0 ; i < 5; i++){
+        printf("%d ",arr[i]); 
     }
-    printf("There are total %d Odd Numbers \n",(5-e_count)); 
-    printf("The Odd Array elements are : \n");
-     for(int i = 0 ; i < 5 ; i++){
-        if(odd[i]==0){
-            continue; 
-        }
-        else{
-        printf("%d\n",odd[i]); 
-        }
-        
-    
-    }
-
-    
 
     return 0;
 }
