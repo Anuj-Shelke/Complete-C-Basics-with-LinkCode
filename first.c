493f354e8797a9859ef4f9e3e86c4a5b29ed4293
Anuj Shelke <anujshelke18@gmail.com>
2026-08-12 18:37:45 +0530

    Code to Print Circular Array

--- first.c
+++ first.c
@@ -2,21 +2,23 @@
 #include <stdio.h>
 
 int main() {
-    int temp = 0 ; 
-    int arr[5]={6,5,4,3,2}; 
-    for(int i = 0 ; i < 5-1 ; i++){
-        for(int j = i+1; j < 5 ; j++){
-            if(arr[j] < arr[i]){
-                temp = arr[j]; 
-                arr[j] = arr[i]; 
-                arr[i] = temp; 
+     int arr[5]={1,2,3,4,5};
+     int n = 5; 
 
-            }
+     int j = 0 ; 
+     int i = 0 ; 
+     int k = 0 ; 
+     for(i =0 ; i < 5 ; i++){
+        int temp = arr[n-1];
+        for(j =n-1 ; j > 0 ;j--){
+            arr[j] = arr[j-1]; 
         }
-    }
-    for(int i =0 ; i < 5; i++){
-        printf("%d ",arr[i]); 
-    }
+        arr[j] = temp; 
+        for(k= 0 ; k< 5; k++ ){
+            printf(" %d",arr[k]); 
+        }
+        printf("\n"); 
+     }
 
     return 0;
 }
