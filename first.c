792af91fb1e990da589dd8e11bfd10fb2da890e5
Anuj Shelke <anujshelke18@gmail.com>
2026-08-17 19:23:47 +0530

    COde to add two 2D Arrays

--- first.c
+++ first.c
@@ -3,12 +3,39 @@
 
 int main() {
     int i,j; 
-    int arr[3][3]={{1,2,3},
-                    {4,5,6},
-                    {7,8,9}}; 
+    
+    int arr[3][3]; 
+    int arr1[3][3]; 
+    int arr3[3][3]; 
+    printf("Enter the matrix elements \n"); 
+
+    
     for(i = 0 ; i <3 ; i++){
         for(j = 0 ; j <3; j++){
-            printf("%d ",arr[i][j]); 
+            scanf("%d",&arr[i][j]);  
+        }
+       
+    }
+    printf("Enter elements of Array 2 \n"); 
+    
+    for(i = 0 ; i <3 ; i++){
+        for(j = 0 ; j <3; j++){
+            scanf("%d",&arr1[i][j]);  
+        }
+       
+    }
+
+    printf("The sum of the Array elements is \n");
+     for(i = 0 ; i <3 ; i++){
+        for(j = 0 ; j <3; j++){
+            arr3[i][j] = arr[i][j]+arr1[i][j]; 
+        }
+         
+    }
+    
+     for(i = 0 ; i <3 ; i++){
+        for(j = 0 ; j <3; j++){
+            printf("%d ",arr3[i][j]);  
         }
         printf("\n"); 
     }
