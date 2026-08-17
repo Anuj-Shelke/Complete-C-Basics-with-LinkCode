1a95328a8fecee174711257cb999d560ae7095ae
Anuj Shelke <anujshelke18@gmail.com>
2026-08-17 18:59:13 +0530

    Multi Dimensional Array in C 2D

--- first.c
+++ first.c
@@ -2,49 +2,15 @@
 #include <stdio.h>
 
 int main() {
-    int arr[10]; 
-    int set_1[5]; 
-    int set_2[5];
-    int count = 0;  
-    int flag = 0 ; 
-    int i = 0 ;
-    int j = 0 ;  
-    printf("Enter Elements for Array one \n"); 
-    for(i = 0 ; i < 5; i++){
-        scanf("%d",&set_1[i]); 
-    }
-    printf("Enter Elements for Array two \n"); 
-    for(j =0 ; j < 5; j++){
-        scanf("%d",&set_2[j]); 
-    }
-
-    // for(i = 0 ; i < 5; i++){
-    //     arr[i] = set_1[i]; 
-
-    // }
-    for(i = 0 ; i < 5; i++){
-        flag = 0 ; 
-        for(j = 0 ;  j < 5; j++){
-            if(set_1[j] == set_2[i]){
-                flag = 1; 
-                
-                break; 
-            }
+    int i,j; 
+    int arr[3][3]={{1,2,3},
+                    {4,5,6},
+                    {7,8,9}}; 
+    for(i = 0 ; i <3 ; i++){
+        for(j = 0 ; j <3; j++){
+            printf("%d ",arr[i][j]); 
         }
-
-          if(flag == 1){
-        arr[count] = set_2[i];
-        count++; 
-          
-            
-        }
-        
-    }
-  
-    
-    printf("The  Intersection of the array element is  : "); 
-    for(int i = 0 ; i< count; i++){
-        printf("%d ",arr[i]); 
+        printf("\n"); 
     }
 
     return 0;
