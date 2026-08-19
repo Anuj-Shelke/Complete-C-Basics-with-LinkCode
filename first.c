89ab36fd302b099e2f22b9dac0d5a83af1d5ce66
Anuj Shelke <anujshelke18@gmail.com>
2026-08-19 19:11:38 +0530

    Code to Addition of two numbers using pointers

--- first.c
+++ first.c
@@ -1,58 +1,19 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-#define max 20 
+// Code to print addition of two numbers using pointers 
+#include<stdio.h>
+
 int main() {
-    int r1,r2;
-    int c1,c2;  
-    printf("Enter number of rows and columns \n");
-    scanf("%d%d",&r1,&c1); 
+    int num = 5;
+    int num1 = 5; 
+    int *p1 = &num;
+    int *p2 = &num1 ;
+    int sum =  *p1+*p2;
 
-    printf("Enter number of rows and columns \n");
-    scanf("%d%d",&r2,&c2); 
+    // printf("%d \n",num); 
+    // printf("%d \n",*p1);
+    // printf("%d \n",*p2);
+    printf("%u \n",sum);
 
-    int i,j; 
-    
-    int arr[max][max]; 
-    int arr1[max][max]; 
-    int arr3[max][max]; 
-  
 
-    if(r1 == r2 && c1==c2){
-    printf("Enter number of rows and columns \n");
-    scanf("%d%d",&r1,&r2); 
-    for(i = 0 ; i <r1 ; i++){
-        for(j = 0 ; j <c1; j++){
-            scanf("%d",&arr[i][j]);  
-        }
-       
-    }
-    printf("Enter elements of Array 2 \n"); 
-    
-    for(i = 0 ; i <r2 ; i++){
-        for(j = 0 ; j <c2; j++){
-            scanf("%d",&arr1[i][j]);  
-        }
-       
-    }
 
-    printf("The sum of the Array elements is \n");
-     for(i = 0 ; i <r1 ; i++){
-        for(j = 0 ; j <c1; j++){
-            arr3[i][j] = arr[i][j]+arr1[i][j]; 
-        }
-         
-    }
-    
-     for(i = 0 ; i <r1 ; i++){
-        for(j = 0 ; j <c1; j++){
-            printf("%d ",arr3[i][j]);  
-        }
-        printf("\n"); 
-    }
-
-    }
-    else{
-        printf("Enter valid numbers \n"); 
-    }
     return 0;
 }
