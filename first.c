753e4353bb1047c7e159ae584064d54e0b847028
Anuj Shelke <anujshelke18@gmail.com>
2026-09-09 22:38:29 +0530

    Code for Insertion Sort:

--- first.c
+++ first.c
@@ -1,23 +1,24 @@
-// Selection Sort Code 
+// Code for insertion Sort 
 #include <stdio.h>
 
 int main() {
-    int temp = 0 ; 
-    int min = 0 ; 
-    int arr[6] = {5,6,4,3,2,1}; 
+    int j = 0 ; 
+    int arr[5] = {1,5,4,3,2}; 
+    int key = 0 ; 
     int n = sizeof(arr)/sizeof(arr[0]); 
-    for(int i= 0 ; i < n-1 ; i++){
-        min = i ; 
-        for(int j = i+1 ; j < n ; j++){
-            if(arr[min] > arr[j]){
-                temp = arr[j]; 
-                arr[j] = arr[min]; 
-                arr[min] = temp; 
-            }
+    for(int i =1 ; i < n ; i++){
+        key = arr[i]; 
+        j = i-1; 
+        while( j > 0 && arr[j]>key){
+            arr[j+1] = arr[j]; 
+            j--; 
+            
         }
+        arr[j+1] = key; 
     }
     for(int i = 0 ; i < n ; i++){
         printf("%d ",arr[i]); 
     }
 
+    return 0;
 }
