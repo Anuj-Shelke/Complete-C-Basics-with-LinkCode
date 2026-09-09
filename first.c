a87db290eb5e0114f725dc71c2411fce1831b92c
Anuj Shelke <anujshelke18@gmail.com>
2026-09-09 10:00:54 +0530

    Binary Search Code DSA C

--- first.c
+++ first.c
@@ -1,21 +1,31 @@
-// Online C Linear Search of Element 
+
 #include <stdio.h>
 
 int main() {
-    int element = 0; 
-    int arr[6] = {1,2,3,4,5,6}; 
-    printf("Enter element to search for "); 
-    scanf("%d",&element);
-    
-    for(int i = 0 ; i < sizeof(arr); i++){
-        if(element = arr[i]){
-    printf("The element %d is found at index %d",arr[i],i);
-            return 0; 
+   int arr[5] = {1,2,3,4,5}; 
+    int element = 10; 
+    int low = 0 ; 
+    int found = 0 ; 
+    int high = sizeof(arr)/sizeof(arr[0]-1); 
+    while(high>= low){
+        int mid = (low+high)/2; 
+        if(arr[mid] == element){
+            printf("The element %d is found at index %d",arr[mid],mid);
+            found = 1; 
+            break; 
         }
-        else {
-                printf("The element not found "); 
+        else if(arr[mid]<element){
+             low = mid+1;
         }
+        else{
+            high = mid-1; 
+        }
+        
     }
-
+    if(found== 0){
+        printf("The element not found in the array "); 
+    }
+    
     return 0;
 }
+    
