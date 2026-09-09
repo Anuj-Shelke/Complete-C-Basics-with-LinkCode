59d41f4085e1411eea9b20807a0b5b713e73bc02
Anuj Shelke <anujshelke18@gmail.com>
2026-09-09 21:38:05 +0530

    Code For Selection Sort :

--- first.c
+++ first.c
@@ -1,25 +1,23 @@
-// Code for Bubble Sort ; 
-
+// Selection Sort Code 
 #include <stdio.h>
 
 int main() {
-  int arr[6] = {6,5,4,3,2,1}; 
-  int size = sizeof(arr)/sizeof(arr[0]); 
-  for(int i = 0 ; i < size-1; i++ ){
-      for(int j =0 ; j < size-i-1; j++){
-          if(arr[j+1]<arr[j]){
-              int temp = arr[j]; 
-              arr[j] = arr[j+1]; 
-              arr[j+1] = temp; 
-          }
-        
-      }
-     
-      
-  }
-     printf("The sorted array is : "); 
-      for(int i = 0 ; i < size; i++){
-          printf("%d ",arr[i]); 
-      }
+    int temp = 0 ; 
+    int min = 0 ; 
+    int arr[6] = {5,6,4,3,2,1}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    for(int i= 0 ; i < n-1 ; i++){
+        min = i ; 
+        for(int j = i+1 ; j < n ; j++){
+            if(arr[min] > arr[j]){
+                temp = arr[j]; 
+                arr[j] = arr[min]; 
+                arr[min] = temp; 
+            }
+        }
+    }
+    for(int i = 0 ; i < n ; i++){
+        printf("%d ",arr[i]); 
+    }
+
 }
-    
