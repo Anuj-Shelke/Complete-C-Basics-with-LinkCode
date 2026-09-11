03b6053467290c62169a95ec18f5f3b18560d521
Anuj Shelke <anujshelke18@gmail.com>
2026-09-11 18:59:52 +0530

    Code For Quick Sort:

--- first.c
+++ first.c
@@ -1,24 +1,54 @@
-// Code for insertion Sort 
 #include <stdio.h>
 
-int main() {
-    int j = 0 ; 
-    int arr[5] = {1,5,4,3,2}; 
-    int key = 0 ; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-    for(int i =1 ; i < n ; i++){
-        key = arr[i]; 
-        j = i-1; 
-        while( j > 0 && arr[j]>key){
-            arr[j+1] = arr[j]; 
-            j--; 
-            
+// Partition function
+int pivot(int arr[], int start, int end) {
+    int pivot = arr[end];   // choose last element as pivot
+    int pindex = start;
+
+    for(int i = start; i < end; i++) {
+        if(arr[i] <= pivot) {
+            int temp = arr[i];
+            arr[i] = arr[pindex];
+            arr[pindex] = temp;
+            pindex++;
         }
-        arr[j+1] = key; 
     }
-    for(int i = 0 ; i < n ; i++){
-        printf("%d ",arr[i]); 
+    // swap pivot into correct position
+    int temp = arr[pindex];
+    arr[pindex] = arr[end];
+    arr[end] = temp;
+
+    return pindex;
+}
+
+// QuickSort recursive function
+void partition(int arr[], int start, int end) {
+    if(start < end) {
+        int pindex = pivot(arr, start, end);
+        partition(arr, start, pindex - 1);
+        partition(arr, pindex + 1, end);
     }
+}
+
+// Display function
+void display(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {2,4,5,1,3};
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Before Sorting: ");
+    display(arr, len);
+
+    partition(arr, 0, len-1);
+
+    printf("After Sorting: ");
+    display(arr, len);
 
     return 0;
 }
